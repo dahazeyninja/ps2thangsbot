c358a8b82593f8ece5df4f7cca5e4b8ac5b9d394
dahazeyninja <dahazeyniinja@gmail.com>
2016-03-20 07:46:15 -0400

    added example config.js

---
+++ config-example.js
@@ -0,0 +1,41 @@
+module.exports.serviceid = 'example'
+
+//  ck = consumer_key
+//  cs = consumer_secret
+//  at = access_token
+//  ats = access_token_secret
+
+module.exports.briggs_ck = 'example'
+module.exports.briggs_cs = 'example'
+module.exports.briggs_at = 'example'
+module.exports.briggs_ats = 'example'
+
+module.exports.connery_ck = 'example'
+module.exports.connery_cs = 'example'
+module.exports.connery_at = 'example'
+module.exports.connery_ats = 'example'
+
+module.exports.cobalt_ck = 'example'
+module.exports.cobalt_cs = 'example'
+module.exports.cobalt_at = 'example'
+module.exports.cobalt_ats = 'example'
+
+module.exports.emerald_ck = 'example'
+module.exports.emerald_cs = 'example'
+module.exports.emerald_at = 'example'
+module.exports.emerald_ats = 'example'
+
+module.exports.miller_ck = 'example'
+module.exports.miller_cs = 'example'
+module.exports.miller_at = 'example'
+module.exports.miller_ats = 'example'
+
+module.exports.ceres_ck = 'example'
+module.exports.ceres_cs = 'example'
+module.exports.ceres_at = 'example'
+module.exports.ceres_ats = 'example'
+
+module.exports.genudine_ck = 'example'
+module.exports.genudine_cs = 'example'
+module.exports.genudine_at = 'example'
+module.exports.genudine_ats = 'example'
